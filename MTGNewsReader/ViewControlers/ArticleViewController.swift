21280c787896f764889e541e6c676907aa80338e
Martin Andonoski <martin.andonoski@spoiledmilk.com>
2015-11-03 21:44:33 +0100

    Loader added to webView

--- MTGNewsReader/ViewControlers/ArticleViewController.swift
+++ MTGNewsReader/ViewControlers/ArticleViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ArticleViewController: UIViewController {
+class ArticleViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView : UIWebView!
+    @IBOutlet weak var activityIndicator : UIActivityIndicatorView!
     
     var item : DefaultFeedItem? = nil
     
@@ -20,6 +21,14 @@ class ArticleViewController: UIViewController {
         let url : NSURL = NSURL(string: (item?.linkURL)!)!
         let req : NSURLRequest = NSURLRequest(URL : url)
         webView.loadRequest(req)
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        activityIndicator.hidden = false
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        activityIndicator.hidden = true
     }
 
 }
